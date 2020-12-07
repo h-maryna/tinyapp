190a6797236c7778a017efb97c18a26403aa70c9
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-12-07 20:08:24 +0000

    Added a new route for a shortURL

--- express_server.js
+++ express_server.js
@@ -22,14 +22,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/set", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
+/**app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
  });
  
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+ });**/
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
