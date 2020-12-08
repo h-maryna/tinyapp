909bb59c8eeb0a54519909e33a9e208805cd9c93
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-12-08 15:19:28 +0000

    Added a post route for submitting a form

--- express_server.js
+++ express_server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2, 6);
+};
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,30 +16,41 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
+app.post("/urls/", (req, res) => {
+  let shortURL = generateRandomString();
+  console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(urlDatabase[shortURL]);
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 
-/**app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });**/
-
+});
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  if (!Object.prototype.hasOwnProperty.call(urlDatabase,shortURL)) {
+    res.status(404);
+    res.send("404 NOT FOUND");
+  } else {
+    const longURL = urlDatabase[shortURL];
+    res.redirect(longURL);
+  }
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
